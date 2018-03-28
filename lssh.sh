ce638c0f99a12995c6dce61ec717b15c98ad9b27
汪小康 <steven.wang@irootech.com>
2018-03-28 11:25:52 +0800

    lssh脚本加上系统判断

--- lssh.sh
+++ lssh.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-basepath=`readlink -f /usr/local/bin/lssh`
+os=`uname`
+echo -e "\n You are using "${os}" OS \n"
+if [ ${os}=="Darwin" ]; then
+    basepath=`readlink /usr/local/bin/lssh`
+else
+    basepath=`readlink -f /usr/local/bin/lssh`
+fi
 python3 `dirname ${basepath}`/main.py
